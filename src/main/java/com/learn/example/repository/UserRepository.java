d660f1837485085f842dd3b223a76558985465e1
abhinav <abhinav.sri2009@gmail.com>
2017-03-24 20:51:47 +0530

    added methods for the

--- src/main/java/com/learn/example/repository/UserRepository.java
+++ src/main/java/com/learn/example/repository/UserRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 public interface UserRepository extends CrudRepository<User, Long> {
 	List<User> findUsersByUsernameAndPassword(String u, String p);
-	User findFirstByUsernameAndPassword(String u, String p);
+
+    User findFirstByUsernameAndPassword(String u, String p);
 
     List<User> findUsersByIdIn(List<Long> userIdList);
+
     List<User> findByCarSetIn(List<Car> cars);
 
 }
