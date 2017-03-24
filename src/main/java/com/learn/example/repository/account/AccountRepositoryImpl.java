f8cbfc2c9438bf8cb2ebbb219846be770c30794d
abhinav <abhinav.sri2009@gmail.com>
2017-03-24 06:14:42 +0530

    made  a method transactional

--- src/main/java/com/learn/example/repository/account/AccountRepositoryImpl.java
+++ src/main/java/com/learn/example/repository/account/AccountRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.learn.example.repository.account;
 
 import com.learn.example.model.Account;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,10 +16,10 @@ public class AccountRepositoryImpl implements AccountRepositoryCustom {
     @PersistenceContext
     EntityManager entityManager;
 
+    @Transactional(readOnly = true)
     @Override
     public List<Account> getAccountsByName(String name) {
-        Query query = entityManager.createQuery("SELECT acc FROM Account as acc WHERE acc.name LIKE:searchName" /*+
-                "WHERE acc.name LIKE ?"*/, Account.class);
+        Query query = entityManager.createQuery("SELECT acc FROM Account as acc WHERE acc.name LIKE:searchName" , Account.class);
         query.setParameter("searchName", "%" + name + "%");
         return query.getResultList();
     }
